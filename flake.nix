ca8276a4a40f7ba07e55fa598d916a233d72c4c6
KiberBomzh <klimenokandrej193@gmail.com>
2026-09-12 23:41:18 +0300

    added package in flake.nix (untested)

--- flake.nix
+++ flake.nix
@@ -8,6 +8,20 @@
 	flake-utils.lib.eachDefaultSystem (system:
 		let pkgs = import nixpkgs { inherit system; };
 		in {
+			packages.default = pkgs.rustPlatform.buildRustPackage {
+				name = "lsfb2";
+				version = "0.1.0";
+
+				src = ./.;
+				cargoLock.lockFile = ./Cargo.lock;
+
+				buildPhase = "cargo build --release --features zip";
+				installPhase = ''
+					mkdir -p $out/bin
+					cp target/release/lsfb2 $out/bin/
+				'';
+			};
+
 			devShells.default = pkgs.mkShell {
 				buildInputs = with pkgs; [
 					rustc
